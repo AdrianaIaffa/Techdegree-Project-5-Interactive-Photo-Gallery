f2d30e8d4dd87ec1589acd3f500a440dca1eae0c
AdrianaIaffa <adrianaiaffa@hotmail.com>
2022-07-18 16:13:06 +0100

    final edit before submition

--- js/app.js
+++ js/app.js
@@ -1,15 +1,4 @@
-// baguetteBox.run('.gallery');
 
-
-// window.addEventListener('load', function() {
-//   baguetteBox.run('.gallery');
-// });
-//
-// const gallery = baguetteBox.run('.gallery');
-// baguetteBox.show(index, gallery[0]);
-//
-// const search = new Filter('search', 'data-caption');​in
-
-baguetteBox.run('.gallery');
+baguetteBox.run('.grid');
 
 const search = new Filter('search', 'data-caption');
