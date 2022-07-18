11a38e97f267f3a4b39b85aabb0d3edb4d1811f9
AdrianaIaffa <adrianaiaffa@hotmail.com>
2022-07-18 14:20:46 +0100

    baguettebox min and app update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
+// baguetteBox.run('.gallery');
+
+
+// window.addEventListener('load', function() {
+//   baguetteBox.run('.gallery');
+// });
+//
+// const gallery = baguetteBox.run('.gallery');
+// baguetteBox.show(index, gallery[0]);
+//
+// const search = new Filter('search', 'data-caption');​in
+
 baguetteBox.run('.gallery');
 
-
-window.addEventListener('load', function() {
-  baguetteBox.run('.gallery');
-});
-
-const gallery = baguetteBox.run('.gallery');
-baguetteBox.show(index, gallery[0]);
-
-const search = new Filter('search', 'data-caption');​in
+const search = new Filter('search', 'data-caption');
